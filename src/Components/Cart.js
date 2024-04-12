21876870f1706abe440b5bd436193f234bcf3703
Xelaj <ajuillard69@gmail.com>
2024-04-12 13:38:51 +0200

    feat(Cart): add banner & cart components with styles

---
+++ src/Components/Cart.js
@@ -0,0 +1,19 @@
+import '../Styles/Cart.css'
+
+const Cart = () => {
+    const monsteraPrice = 8;
+    const lierrePrice = 10;
+    const bouquetFleurs = 15;
+    return (
+      <div className="lmj-cart">
+      <ul>
+        <li>Monstera : {monsteraPrice} €</li>
+        <li>Lierre : {lierrePrice} €</li>
+        <li>Bouquet de fleurs : {bouquetFleurs} €</li>
+      </ul>
+      <p>Total panier : {monsteraPrice + lierrePrice + bouquetFleurs} €</p>
+      </div>
+    );
+  };
+
+  export default Cart;
